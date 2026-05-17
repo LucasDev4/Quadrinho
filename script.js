1da94c25b468e657e4e3cffa9d84abd3928fac85
LucasRodrigues <rodriguessslucas68@gmail.com>
2026-05-17 12:50:53 -0300

    Second Commit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const dots = document.querySelectorAll('.counting');
     const prevBtn = document.querySelectorAll('.pass img')[0];
     const nextBtn = document.querySelectorAll('.pass img')[1];
+    const comment = document.getElementById("comment");
 
     let currentIndex = 0;
 
@@ -39,6 +40,20 @@ document.addEventListener("DOMContentLoaded", () => {
             currentIndex = images.length - 1;
         }
         updateCarousel();
+    });
+
+    comment.addEventListener("click", () => {
+        const currentImg = images[currentIndex];
+        
+        let currentSrc = currentImg.src;
+
+        if (currentSrc.includes("SemBalao")) {
+            currentImg.src = currentSrc.replace("SemBalao", "");
+        } 
+  
+        else {
+            currentImg.src = currentSrc.replace(".png", "SemBalao.png");
+        }
     });
 
     dots.forEach((dot, index) => {
